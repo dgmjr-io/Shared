ae19db02382bf17f7dec6270989882f0cae45ec6
David G. Moore, Jr <dev@dgmjr.io>
2023-10-12 09:11:28 -0400

    Update submodules

--- Invoke-SlnGen/Invoke-SlnGen.ps1
+++ Invoke-SlnGen/Invoke-SlnGen.ps1
@@ -1,38 +1,50 @@
+using assembly "/usr/local/share/PackageManagement/NuGet/Packages/microsoft.build.17.7.2/lib/net7.0/Microsoft.Build.dll"
+
 [CmdletBinding()]
-  param(
-    [Parameter(ValueFromPipeline,
-    ValueFromPipelineByPropertyName,
-    Mandatory,
-    Position = 1,
-    ParameterSetName = "Default",
-    HelpMessage = "The path to the project file to build the solution for. Defaults to the first .*proj file in the current directory.")]
-    [string]
-    $ProjectPath
-  )
+param(
+  [Parameter(ValueFromPipeline,
+  ValueFromPipelineByPropertyName,
+  Mandatory,
+  Position = 1,
+  ParameterSetName = "Default",
+  HelpMessage = "The path to the project file to build the solution for. Defaults to the first .*proj file in the current directory.")]
+  [string]$ProjectPath
+)
 
-  begin {
-    $ProjectFileExists = [System.IO.File]::Exists($ProjectPath);
+begin {
+  $ProjectPath = [System.IO.Path]::GetFullPath($ProjectPath)
+  $ProjectFileExists = [System.IO.File]::Exists($ProjectPath);
 
-    Set-Location $([System.IO.Path]::GetDirectoryName($ProjectPath))
+  Set-Location $(Split-Path $ProjectPath)
 
-    Write-Debug("Processing project file $ProjectPath...");
-    Write-Debug("File exists: ${ProjectFileExists ? "YES!" : "NO"}...");
-    if (!$ProjectFileExists) {
-      Write-Error("Project file $ProjectPath does not exist.");
-      return;
-    }
+  Write-Debug("Processing project file $ProjectPath...");
+  Write-Debug("File exists: ${ProjectFileExists ? "YES!" : "NO"}...");
+  if (!$ProjectFileExists) {
+    Write-Error("Project file $ProjectPath does not exist.");
+    exit 1;
   }
+}
 
-  process {
-    slngen --launch false $ProjectPath
-    $project = [System.Xml.Linq.XDocument]::Load($ProjectPath)
-    $projectReferences = $project.Descendants("ProjectReference")
-    $projectReferences | ForEach-Object {
-      $projectReference = $_
-      $projectReferencePath = $projectReference.Attribute("Include").Value
+process {
+  slngen --launch false "$ProjectPath"
+  # [Microsoft.Build.Execution.ProjectInstance]$project = [Microsoft.Build.Execution.ProjectInstance]::FromFile($ProjectPath)
+  [System.Xml.Linq.XDocument]$project = [System.Xml.Linq.XDocument]::Load($ProjectPath)
+  $projectReferences = $project.Descendants("ProjectReference")
+  $projectReferences | ForEach-Object {
+    $projectReference = $_
+    $projectReferencePath = $projectReference.Attribute("Include").Value
+    $includeInSolutionFile = [bool]::Parse($projectReference.Attribute("IncludeInSolutionFile")?.Value ?? "true")
+    if($includeInSolutionFile)
+    {
       $projectReferenceFullPath = Join-Path (Split-Path $ProjectPath) $projectReferencePath
       Write-Debug("Processing project reference $projectReferenceFullPath...")
       dotnet sln add $projectReferenceFullPath
     }
-    dotnet sln add $ProjectPath
   }
+
+  $includeInSolutionFile = [bool]::Parse($project.Descendants("IncludeInSolutionFile")?.Value ?? "true")
+  if($includeInSolutionFile)
+  {
+    dotnet sln add "$ProjectPath"
+  }
+}
